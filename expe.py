5fd83e5101d7af521de2fccf83dd0eaa714dd0fb
Ogawa <rst34shimizuku@stu.kanazawa-u.ac.jp>
2024-04-27 13:05:15 +0900

    whiletrue

--- expe.py
+++ expe.py
@@ -7,14 +7,16 @@ import speech_recognition as sr
 import whisper
 model = whisper.load_model("base")
 r = sr.Recognizer()
-with sr.Microphone(sample_rate=16_000) as source:
-    print("なにか話してください")
-    audio = r.listen(source)
+while True:
+    with sr.Microphone(sample_rate=16_000) as source:
+        print("なにか話してください")
+        audio = r.listen(source)
 
-    print("音声処理中 ...")
-    wav_bytes = audio.get_wav_data()
-    wav_stream = BytesIO(wav_bytes)
-    audio_array, sampling_rate = sf.read(wav_stream)
-    audio_fp32 = audio_array.astype(np.float32)
+        print("音声処理中 ...")
+        wav_bytes = audio.get_wav_data()
+        wav_stream = BytesIO(wav_bytes)
+        audio_array, sampling_rate = sf.read(wav_stream)
+        audio_fp32 = audio_array.astype(np.float32)
 
-    result = model.transcribe(audio_fp32, fp16=False)
+        result = model.transcribe(audio_fp32, fp16=False)
+        print(result["text"])
