fac2785e9505af2db4ef828b791e68b2f0d9589a
Ogawa <rst34shimizuku@stu.kanazawa-u.ac.jp>
2024-05-02 13:46:05 +0900

    shことはじめ

---
+++ getstarted.sh
@@ -0,0 +1,20 @@
+# おやくそく
+sudo apt update -y
+sudo apt upgrade -y
+
+# Python構築
+sudo apt install python3-pip -y #要確認
+pip install --upgrade pip
+
+pip install requests
+pip install json
+pip install pygame
+pip install whisper-openai
+pip install threading
+pip install soundcard
+pip install soundfile
+pip install numpy
+
+# ボイボ
+curl https://github.com/VOICEVOX/voicevox/releases/download/0.19.1/voicevox-linux-cpu-0.19.1.tar.gz
+tar -xvf voicevox-linux-cpu-0.19.1.tar.gz
