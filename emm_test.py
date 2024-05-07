80740bfb647a2d19541b86b564dddaf22a4db496
Ogawa <rst34shimizuku@stu.kanazawa-u.ac.jp>
2024-05-07 18:30:05 +0900

    print

--- emm_test.py
+++ emm_test.py
@@ -5,6 +5,15 @@ import requests
 
 while True:
     tts.synthesize_speech("こんにちは。")
-    time.sleep(5)
+    time.sleep(1)
+    print("1")
+    time.sleep(1)
+    print("2")
+    time.sleep(1)
+    print("3")
+    time.sleep(1)
+    print("4")
+    time.sleep(1)
+    print("5")
 
 
