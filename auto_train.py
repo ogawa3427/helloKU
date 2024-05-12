3ea9274eba7c14807153c5affc04f1adfff86f36
Ogawa <rst34shimizuku@stu.kanazawa-u.ac.jp>
2024-05-12 21:47:33 +0900

    x

--- auto_train.py
+++ auto_train.py
@@ -1,11 +1,12 @@
 import pyautogui
 import time
-
+pyautogui.FAILSAFE = True
 # 画面の解像度を取得
 screenWidth, screenHeight = pyautogui.size()
-
+print(screenWidth)
+print(screenHeight)
 # マウスを画面中央に移動
-pyautogui.moveTo(screenWidth / 2, screenHeight / 2)
+pyautogui.moveTo(screenWidth / 2, screenHeight / 2, 2)
 
 # マウスを左クリック
 pyautogui.click()
@@ -14,11 +15,9 @@ pyautogui.click()
 pyautogui.write('Hello, world!', interval=0.25)
 
 # スクリーンショットを取り、ファイルに保存
-screenshot = pyautogui.screenshot()
-screenshot.save('screenshot.png')
+#screenshot = pyautogui.screenshot()
+#screenshot.save('screenshot.png')
 
-# 5秒待機
-time.sleep(5)
 
 # マウスを画面の左上に移動
-pyautogui.moveTo(0, 0)
+pyautogui.moveTo(0, 0, 2)
---
+++ xgetstarted.sh
@@ -0,0 +1,8 @@
+pip install pyautogui
+
+echo 'export DISPLAY=$(grep nameserver /etc/resolv.conf | awk '{print $2}'):0.0' >> ~/.bashrc
+source ~/.bashrc
+
+touch ~/.Xauthority
+xauth generate $DISPLAY . trusted
+
