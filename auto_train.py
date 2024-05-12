8806a6b2a0b5e01a1e7671c6c5f10af5367c809f
Ogawa <rst34shimizuku@stu.kanazawa-u.ac.jp>
2024-05-12 23:08:37 +0900

    みせ

--- auto_train.py
+++ auto_train.py
@@ -18,6 +18,25 @@ pyautogui.write('Hello, world!', interval=0.25)
 #screenshot = pyautogui.screenshot()
 #screenshot.save('screenshot.png')
 
+import pyautogui
+import time
+from PIL import Image, ImageDraw
 
+pyautogui.FAILSAFE = True
+screenWidth, screenHeight = pyautogui.size()
+print(screenWidth)
+print(screenHeight)
+pyautogui.moveTo(screenWidth / 2, screenHeight / 2, 2)
+pyautogui.click()
+pyautogui.write('Hello, world!', interval=0.25)
+
+# マウスの位置に基づいて画像を生成し表示するループ
+while True:
+    x, y = pyautogui.position()
+    img = Image.new('RGB', (100, 100), color = (255, 255, 255))
+    draw = ImageDraw.Draw(img)
+    draw.ellipse((40, 40, 60, 60), fill=(255, 0, 0))
+    img.show()
+    time.sleep(1)  # 1秒ごとに画像を更新
 # マウスを画面の左上に移動
 pyautogui.moveTo(0, 0, 2)
